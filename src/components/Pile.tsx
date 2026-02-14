71e0ce7fac6f562416047be3da42f2159c9f14e6
Daniel Whiffing <danielwhiffing@gmail.com>
2026-02-14 00:10:05 -0500

    Adds foundation labels

--- src/components/Pile.tsx
+++ src/components/Pile.tsx
@@ -10,5 +10,11 @@ export const Pile = ({
     className={`pile ${pileType}`}
     data-pileindex={pileIndex}
     data-piletype={pileType}
-  />
+  >
+    {pileType === 'foundation' && (
+      <div className="text-[#fff4] absolute inset-0 flex items-center justify-center pointer-events-none text-[calc(var(--base-size)*0.4)]">
+        0/9
+      </div>
+    )}
+  </div>
 )
